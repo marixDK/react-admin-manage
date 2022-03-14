849ab6e28fb9b17e82e7fb66cd31bcbe6afb58d8
baojunGit <1018440268@qq.com>
2022-03-14 15:19:28 +0800

    2022-3-14 #comment 测试commitlint是否生效  #time 3h

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "@/style/index.scss"
 /**
  * 需要在最顶层添加BrowserRouter，它提供了一个历史上下文，才能使用useRoutes()
  */
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>,
   document.getElementById("root")
