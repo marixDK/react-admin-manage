8658c517dd616636262471a55d0373954c606d19
baojunGit <1018440268@qq.com>
2022-03-14 15:06:33 +0800

    2022-3-14 #comment 测试eslint和prettier配置是否生效 #time 3h

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import "@/style/index.scss";
+import ReactDOM from "react-dom"
+import App from "./App"
+import "@/style/index.scss"
 /**
  * 需要在最顶层添加BrowserRouter，它提供了一个历史上下文，才能使用useRoutes()
  */
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>,
   document.getElementById("root")
