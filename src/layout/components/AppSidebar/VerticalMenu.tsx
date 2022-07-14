6692d28165effdc851b074810b1bf55ba72ad56c
baojunGit <1018440268@qq.com>
2022-07-14 14:42:24 +0800

    fix: 解决antd menu异常类型报错

--- src/layout/components/AppSidebar/VerticalMenu.tsx
+++ src/layout/components/AppSidebar/VerticalMenu.tsx
@@ -1,50 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from 'antd';
-import type { MenuProps } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
-type MenuItem = Required<MenuProps>['items'][number];
-
-const items: MenuItem[] = [
+const menuItems = [
   {
     label: '首页',
-    key: '1',
+    key: '/index',
     icon: <i className='iconfont icon-index'></i>,
   }, // 菜单项务必填写 key
   {
     label: '系统管理',
-    key: '2',
+    key: '/sys',
     icon: <i className='iconfont icon-setup'></i>,
     children: [
       {
         label: '用户管理',
-        key: '21',
+        key: '/sys/user',
         icon: '',
       },
       {
         label: '角色管理',
-        key: '22',
+        key: '/sys/role',
         icon: '',
       },
       {
         label: '菜单管理',
-        key: '23',
+        key: '/sys/menu',
         icon: '',
       },
       {
         label: '字典管理',
-        key: '24',
+        key: '/sys/dict',
         icon: '',
       },
     ],
   },
   {
     label: '编辑管理',
-    key: '3',
+    key: '/dict',
     icon: <i className='iconfont icon-edit'></i>,
     children: [
       {
         label: '文章管理',
-        key: '31',
+        key: '/dict/article',
+        icon: '',
+      },
+    ],
+  },
+  {
+    label: '组件管理',
+    key: '/case',
+    icon: <i className='iconfont icon-app'></i>,
+    children: [
+      {
+        label: '滚动组件',
+        key: '/case/scroll',
+        icon: '',
+      },
+      {
+        label: '按钮组件',
+        key: '/case/button',
+        icon: '',
+      },
+      {
+        label: '视频组件',
+        key: '/case/video',
+        icon: '',
+      },
+      {
+        label: '表格',
+        key: '/case/table',
+        icon: '',
+        children: [
+          {
+            label: '模糊查询',
+            key: '/case/table-fuzzy-query',
+            icon: '',
+          },
+        ],
+      },
+      {
+        label: '切割面板',
+        key: '/case/splitPane',
+        icon: '',
+      },
+    ],
+  },
+  {
+    label: '拖拽功能',
+    key: '/drag',
+    icon: <i className='iconfont icon-Group'></i>,
+    children: [
+      {
+        label: '列表拖拽',
+        key: '/drag/list',
+        icon: '',
+      },
+      {
+        label: '表格拖拽',
+        key: '/drag/table',
+        icon: '',
+      },
+      {
+        label: '卡片拖拽',
+        key: '/drag/card',
+        icon: '',
+      },
+    ],
+  },
+  {
+    label: '错误页面',
+    key: '/error',
+    icon: <i className='iconfont icon-warn'></i>,
+    children: [
+      {
+        label: '401页面',
+        key: '/error/401',
+        icon: '',
+      },
+      {
+        label: '404页面',
+        key: '/error/404',
         icon: '',
       },
     ],
@@ -52,16 +128,39 @@ const items: MenuItem[] = [
 ];
 
 const VerticalMenu: React.FC = () => {
-  const goPage = () => {
+  const [selectedKeys, setSelectedKeys] = useState(['/index']);
+  const [openKeys, setOpenKeys] = useState(['/index']);
+  const onOpenChange = (keys) => {
+    // 回调参数：所有展开的submenu的key数组，可以设置只展开最后点击的那项
+    // console.log(keys);
+    setOpenKeys(keys);
+  };
+
+  const navigate = useNavigate();
+  const goPage = ({ item, key, keyPath, domEvent }) => {
+    console.log(key);
     console.log('跳转页面');
+    setSelectedKeys([key]);
+    navigate(key);
   };
   return (
+    // defaultSelectedKeys	初始选中的菜单项 key 数组
+    // selectedKeys 当前选中的菜单项 key 数组
+    // openKeys 当前展开的 SubMenu 菜单项 key 数组，用于设置只展开一项
+    // onOpenChange SubMenu 展开/关闭的回调
     <Menu
       className='vertical-menu'
       mode='inline'
       theme='light'
+      // @ts-ignore
+      // 注释掉莫名的类型错误，也可以在头部用@ts-nocheck，忽略整个文件
+      items={menuItems}
+      onOpenChange={onOpenChange}
+      defaultOpenKeys={openKeys}
+      openKeys={openKeys}
+      defaultSelectedKeys={selectedKeys}
+      selectedKeys={selectedKeys}
       onClick={goPage}
-      items={items}
     />
   );
 };
--- src/routes/asyncRoutes.tsx
+++ src/routes/asyncRoutes.tsx
@@ -3,6 +3,7 @@ import { lazy } from 'react';
 // 在客户端渲染的场景下优先使用React.lazy搭配React.Suspense实现组件懒加载
 const Layout = lazy(() => import('@/Layout'));
 const Index = lazy(() => import('@/views/modules/Index'));
+const User = lazy(() => import('@/views/modules/sys/User'));
 
 // 这里要使用tsx文件，不然使用<Home />组件的时候会报如下错误
 // “Login”表示值，但在此处用作类型。是否指“类型 Login”
@@ -10,16 +11,31 @@ const Index = lazy(() => import('@/views/modules/Index'));
 const asyncRoutes = [
   {
     path: '/',
-    name: 'layout',
+    name: 'Layout',
     key: '/',
     element: <Layout />,
     children: [
       {
         path: 'index',
         name: '首页',
-        key: 'index',
+        key: '/index',
         element: <Index />,
         children: [],
+      },
+      {
+        path: 'sys',
+        name: '系统管理',
+        key: '/sys',
+        element: <User />,
+        children: [
+          {
+            path: 'user',
+            name: '用户管理',
+            key: '/sys/user',
+            element: <User />,
+            children: [],
+          },
+        ],
       },
     ],
   },
