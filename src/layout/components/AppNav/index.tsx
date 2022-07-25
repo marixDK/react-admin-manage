e1fd2d004b43596610f39cfa525b1687b0bb96a5
baojunGit <1018440268@qq.com>
2022-07-26 00:01:14 +0800

    feat: 新增侧边栏hamburger组件

--- src/layout/components/AppNav/index.tsx
+++ src/layout/components/AppNav/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Hamburder from './Hamburger';
 
 const AppNav: React.FC = () => {
-  return <div>顶部导航栏</div>;
+  return (
+    <div className='app-nav'>
+      <Hamburder />
+    </div>
+  );
 };
 
 export default AppNav;
