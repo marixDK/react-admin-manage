8ca9d842d7551364c8f09cd48f092ccc4fd18658
baojunGit <1018440268@qq.com>
2022-07-28 00:39:05 +0800

    feat: 引入ant design栅格布局

--- src/layout/components/AppNav/index.tsx
+++ src/layout/components/AppNav/index.tsx
@@ -1,10 +1,18 @@
+import { Col, Row } from 'antd';
 import React from 'react';
-import Hamburder from './Hamburger';
+import Hamburger from './Hamburger';
 
 const AppNav: React.FC = () => {
   return (
     <div className='app-nav'>
-      <Hamburder />
+      <Row className='nav-row' gutter={16}>
+        <Col xl={12} lg={12} md={12} sm={12} xs={2}>
+          <div className='left-panel'>
+            <Hamburger />
+          </div>
+        </Col>
+        <Col xl={12} lg={12} md={12} sm={12} xs={22}></Col>
+      </Row>
     </div>
   );
 };
