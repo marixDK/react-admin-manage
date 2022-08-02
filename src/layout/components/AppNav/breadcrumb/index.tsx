24676d11ce52fdb6b8e5ea3e514c09128f94dd0e
baojunGit <1018440268@qq.com>
2022-08-02 10:30:36 +0800

    feat: 完成了面包屑显示功能

--- src/layout/components/AppNav/breadcrumb/index.tsx
+++ src/layout/components/AppNav/breadcrumb/index.tsx
@@ -4,32 +4,33 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import asyncRoutes from '@/routes/asyncRoutes';
 import { getRouteItem } from './util';
 
-console.log(asyncRoutes);
-
 const NavBreadcrumb: React.FC = () => {
-  // const [breadcrumbList, setBreadcrumbList] = useState([]);
   const { pathname } = useLocation();
   // 下面的代码相当于这一段注释的简写
   // const breadcrumbList = pathname.split('/').filter((item) => {
   //   return Boolean(item);
   // });
-  const breadcrumbList = pathname.split('/').filter(Boolean);
-  console.log(breadcrumbList);
+  const [breadcrumbList, setBreadcrumbList] = useState([]);
   // const navigate = useNavigate();
   useEffect(() => {
-    console.log(pathname);
-    console.log(getRouteItem(asyncRoutes, pathname));
-  });
+    const routeList = getRouteItem(asyncRoutes, pathname);
+    setBreadcrumbList([...routeList]);
+  }, [pathname]);
   return (
     <Breadcrumb separator='>'>
-      <Breadcrumb.Item>Home</Breadcrumb.Item>
+      {breadcrumbList.map((item) => (
+        <Breadcrumb.Item key={item.path}>
+          <span>{item.name}</span>
+        </Breadcrumb.Item>
+      ))}
+      {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
       <Breadcrumb.Item>
         <span>Application Center</span>
       </Breadcrumb.Item>
       <Breadcrumb.Item>
         <span>Application List</span>
       </Breadcrumb.Item>
-      <Breadcrumb.Item>An Application</Breadcrumb.Item>
+      <Breadcrumb.Item>An Application</Breadcrumb.Item> */}
     </Breadcrumb>
   );
 };
--- src/layout/components/AppNav/breadcrumb/util.ts
+++ src/layout/components/AppNav/breadcrumb/util.ts
@@ -1,8 +1,15 @@
-export const getRouteItem = (arr, queryItem) => {
+export const getRouteItem = (arr, queryItem, list = []) => {
   // ES6 提供三个新的方法 —— entries()，keys()和values() —— 用于遍历数组。它们都返回一个遍历器对象，可以用for...of循环进行遍历，
   // keys()是对键名的遍历、values()是对键值的遍历，entries()是对键值对的遍历。
-  for (const { path, name, children } of arr) {
-    console.log(queryItem);
-    console.log(path);
+  for (const item of arr) {
+    console.log(item);
+    const { path, children } = item;
+    if (queryItem.includes(path)) list.push(item);
+    if (children) {
+      getRouteItem(children, queryItem, list);
+    }
   }
+  // ES6数组过滤filter，过滤掉path为/选项
+  list = list.filter((item) => item.path !== '/');
+  return list;
 };
